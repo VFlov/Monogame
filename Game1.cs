a742ca34da0de7e7fe9aa4a17b7614bbc115e320
VFlov <129610413+VFlov@users.noreply.github.com>
2023-05-25 16:50:54 +0500

    Monogame_v2

--- Game1.cs
+++ Game1.cs
@@ -3,12 +3,10 @@ using Accessibility;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using SharpDX.Direct3D9;
+
 using System;
 using System.Collections.Generic;
-using System.Security.Cryptography.X509Certificates;
 using System.Threading;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
 
 namespace MonoGames2
 {
@@ -17,9 +15,8 @@ namespace MonoGames2
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         SpriteFont MyText;
-        List<Bullet> bullets;
-
-
+        List<Bullet> bullets; //Пули делают бдыщ-бдыщ
+        List<Block> blocks;
 
         Texture2D playerTexture; // наш спрайт
         Texture2D playerTextureStay;
@@ -34,7 +31,9 @@ namespace MonoGames2
         Texture2D[] bulletTexture = new Texture2D[5];
         Texture2D whiteDotTexture;
         Texture2D whiteDotTexture2;
-        Texture2D DialogTexture;
+        Texture2D profSharpTexture;
+        Texture2D spikeTexture;
+        Texture2D blockTexture;
 
         Vector2 black_box;
         Vector2 playerPosition; // позиция нашего спрайта
@@ -43,7 +42,7 @@ namespace MonoGames2
         Vector2 whiteDotPosition2;
         Vector2 healthBarPosition;
         Vector2 healthBarBGPosition;
-        Vector2 DialogPosition;
+        Vector2 profSharpPosition;
 
         Point playerSize; // размер нашего спрайта
         Point evilSpriteSize; // размер второго спрайта
@@ -53,6 +52,8 @@ namespace MonoGames2
         Point frameSizePlayerStay = new Point(6, 0);
         Point framePlayerJumpOrFall = new Point(0, 0);
         Point frameSizeJumpOrFall = new Point(2, 0);
+        Point frameProfSharp = new Point(0, 0);
+        Point frameProfSharpSize = new Point(2, 0);
 
         public SpriteFont DialogFont { get; private set; }
 
@@ -61,6 +62,9 @@ namespace MonoGames2
         const int frameHeight = 85;
         const int playerWidth = 56;
         const int playerHeight = 85;
+
+        const int frameProfSharpWidth = 144;
+        const int frameProfSharpHeigth = 146;
 
         float playerSpeed = 10f;
         float evilSpriteSpeed = 2f;
@@ -79,9 +83,11 @@ namespace MonoGames2
         bool isFalling = false;
         float totalTimer = 0;
         float totalTimer2 = 0;
+        float totalTimer3 = 0;
 
         private readonly float fireRate = 4f; // Количество выстрелов в секунду
 
+        DialogBox dialogbox = new DialogBox();
         Color color = Color.CadetBlue;
         Matrix projectionMatrix; // матрица проекции
         //Matrix viewMatrix; // матрица вида
@@ -101,8 +107,9 @@ namespace MonoGames2
             healthBarBGPosition = new Vector2(16, 16);
             black_box = new Vector2(graphics.PreferredBackBufferWidth - 32, graphics.PreferredBackBufferHeight - 64);
             whiteDotPosition = new Vector2(playerPosition.X, playerPosition.Y);
-            DialogPosition = new Vector2(200, 200);
-
+            profSharpPosition = new Vector2(graphics.PreferredBackBufferWidth-frameProfSharpWidth, 0);
+            dialogbox.Position = new Vector2(graphics.PreferredBackBufferWidth - frameProfSharpWidth - (int)(318*1.5), frameProfSharpHeigth - (int)(78*1.5));
+            dialogbox.Text = "Добро пожаловать в этот чудный и \nнесправедливый мир!" ;
         }
 
         protected override void Initialize()
@@ -111,7 +118,7 @@ namespace MonoGames2
             //projectionMatrix = Matrix.CreateOrthographicOffCenter(0, graphics.PreferredBackBufferHeight, graphics.PreferredBackBufferWidth, 0,0,1);
             bullets = new List<Bullet>();
             //worldMatrix = Matrix.Identity;
-
+            blocks = new List<Block>();
             base.Initialize();
         }
 
@@ -136,7 +143,9 @@ namespace MonoGames2
             MyText = Content.Load<SpriteFont>("MyText");
             whiteDotTexture = Content.Load<Texture2D>("whiteDot");
             whiteDotTexture2 = Content.Load<Texture2D>("whiteDot");
-            DialogTexture = Content.Load<Texture2D>("DialogBoxBackground");
+            dialogbox.Texture = Content.Load<Texture2D>("DialogBoxBackground");
+            profSharpTexture = Content.Load<Texture2D>("FaceSharp");
+            spikeTexture = Content.Load<Texture2D>("Traps/Idle");
             playerTexture = playerTextureStay;
             // Устанавливаем размеры спрайтов
             playerSize = new Point(playerWidth, playerHeight);
@@ -152,6 +161,7 @@ namespace MonoGames2
             KeyboardState keyboardState = Keyboard.GetState();
             totalTimer += (float)gameTime.ElapsedGameTime.TotalSeconds;
             totalTimer2 += gameTime.ElapsedGameTime.Milliseconds;
+            totalTimer3 += gameTime.ElapsedGameTime.Milliseconds;
             //ВЫХОД
             if (keyboardState.IsKeyDown(Keys.Escape))
                 Exit();
@@ -181,7 +191,7 @@ namespace MonoGames2
                 }
             }
             Enemy();
-
+            //Generate();
             foreach (Bullet bullet in bullets)
             {
                 bullet.BulletPosition += new Vector2(bullet.Speed, 0);
@@ -196,7 +206,7 @@ namespace MonoGames2
                 if (health <= 100)
                     health = health + 1;
             }
-            whiteDotPosition = new Vector2(playerPosition.X - 10, playerPosition.Y - 10);
+            whiteDotPosition = new Vector2(profSharpPosition.X, profSharpPosition.Y);
             base.Update(gameTime);
         }
 
@@ -230,23 +240,40 @@ namespace MonoGames2
             {
                 spriteBatch.Draw(bulletTexture[LVL], bullet.BulletPosition, Color.White);
             }
+            foreach (Block block in blocks)
+            {
+                spriteBatch.Draw(block.Texture, block.Position, null, Color.White, 0, Vector2.Zero, 4.0f, SpriteEffects.None, 0);
+            }
             spriteBatch.DrawString(MyText, playerPosition.X.ToString() + "X", new Vector2(10, 120), Color.Azure); // draw text
             spriteBatch.DrawString(MyText, playerPosition.Y.ToString() + "Y " + (float)gameTime.ElapsedGameTime.TotalMilliseconds + "\n" + "Some text", new Vector2(10, 145), Color.Azure);
             spriteBatch.Draw(whiteDotTexture, whiteDotPosition, Color.White);
             spriteBatch.Draw(whiteDotTexture2, whiteDotPosition2, Color.White);
-            spriteBatch.Draw(DialogTexture, DialogPosition, null, Color.White,0,Vector2.Zero, 2.0f,SpriteEffects.None,0);
+            //spriteBatch.Draw(dialogbox.Texture, dialogbox.Position, null, Color.White,0,Vector2.Zero, 2.0f,SpriteEffects.None,0);
+            spriteBatch.Draw(profSharpTexture, profSharpPosition,
+                    new Rectangle(frameProfSharp.X * frameProfSharpWidth,
+                        frameProfSharp.Y * frameProfSharpHeigth,
+                        frameProfSharpWidth, frameProfSharpHeigth),
+                    Color.White);
+            spriteBatch.Draw(dialogbox.Texture, dialogbox.Position, null, Color.White, 0, Vector2.Zero, 1.5f, SpriteEffects.None, 0);
+            spriteBatch.DrawString(MyText, dialogbox.Text, dialogbox.Position + new Vector2(10,10), Color.White);
             spriteBatch.End();
             base.Draw(gameTime);
         }
 
         protected bool Collide()
         {
+            bool collide = false;
             Rectangle goodSpriteRect = new Rectangle((int)playerPosition.X,
                 (int)playerPosition.Y, playerSize.X, playerSize.Y);
             Rectangle evilSpriteRect = new Rectangle((int)evilSpritePosition.X,
                 (int)evilSpritePosition.Y, evilSpriteSize.X, evilSpriteSize.Y);
-
-            return goodSpriteRect.Intersects(evilSpriteRect);
+            foreach (var i in blocks)
+                if (goodSpriteRect.Intersects(new Rectangle((int)i.Position.X, (int)i.Position.Y, i.Texture.Width*4, i.Texture.Height*4)))
+                    collide = true;
+            //RECTANGLE FOR SPIKES
+            if (goodSpriteRect.Intersects(evilSpriteRect)) 
+                collide = true;
+            return collide;
         }
         protected bool Gravity(int position)
         {
@@ -276,6 +303,18 @@ namespace MonoGames2
                     ++framePlayer.Y;
                     if (framePlayer.Y >= frameSize.Y)
                         framePlayer.Y = 0;
+                }
+            }
+            if (totalTimer3 >= 1000)
+            {
+                ++frameProfSharp.X;
+                totalTimer3 = 0;
+                if (frameProfSharp.X >= frameProfSharpSize.X)
+                {
+                    frameProfSharp.X = 0;
+                    ++frameProfSharp.Y;
+                    if (frameProfSharp.Y >= frameProfSharpSize.Y)
+                        frameProfSharp.Y = 0;
                 }
             }
 
@@ -335,11 +374,13 @@ namespace MonoGames2
             {
                 LVL++;
                 playerPosition.X = playerWidth + 10;
+                Generate();
             }
             if (i < 0 && LVL > 0)
             {
                 LVL--;
                 playerPosition.X = 1920 - playerWidth - 10;
+                Generate();
             }
         }
         public void FireBullet(bool turnRight)
@@ -359,6 +400,48 @@ namespace MonoGames2
             Bullet bullet = new Bullet(bulletPosition, velocity);
             bullets.Add(bullet);
         }
+        public void Generate()
+        {
+            switch (LVL)
+            {
+                case 0:
+                    LVL1();
+                    break;
+                case 1:
+                    LVL2();
+                    break;
+                case 2:
+                    LVL3();
+                    break;
+                case 3:
+                    LVL4();
+                    break;
+
+            }
+        }
+        public void LVL1()
+        {
+            blocks.Clear();
+        }
+        public void LVL2()
+        {
+            var spikePosition = new Vector2(graphics.PreferredBackBufferWidth/2, ground + spikeTexture.Height+5);
+            blocks.Clear();
+            for (int i = 0; i < 25; i++)
+            {
+                blocks.Add(new Block(spikeTexture, spikePosition));
+                spikePosition += new Vector2(spikeTexture.Width*2, 0);
+            }
+        }
+        public void LVL3()
+        {
+            blocks.Clear();
+        }
+        public void LVL4()
+        {
+            blocks.Clear();
+        }
+
 
     }
     public class Bullet
@@ -381,6 +464,31 @@ namespace MonoGames2
     {
         public string Text { get; set; }
 
-        public int MaxLength { get; set; }
+        public SpriteFont SpriteF { get; set; }
+
+        //public int MaxLength { get; set; }
+
+        public Texture2D Texture { get; set; }
+        public Vector2 Position { get; set; }
+
+    }
+    public class Block
+    {
+        public Texture2D Texture { get; set; }
+        public Vector2 Position { get; set; }
+
+        public Block(Texture2D texture, Vector2 position)
+        {
+            Texture = texture;
+            Position = position;
+        }
+    }
+    public class Enemy
+    {
+        public Texture2D Texture { get; }
+        public Vector2 Position { get; set; }
+
+        public float Damage { get; }
+        public float Health { get; }
     }
 }
